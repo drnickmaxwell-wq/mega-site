a5d6e53c86d43c51e060f2510dbf2091452672d3
nick maxwell <drnickmaxwell@gmail.com>
2025-09-13 13:49:30 -0700

    Create page.tsxfeat: add about page placeholder

---
+++ ST_MARYS_FRAMEWORK/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div className="mx-auto max-w-container px-4 py-16">
+      <h1 className="font-montserrat font-bold text-4xl mb-4">About Us</h1>
+      <p className="font-lora">This is a placeholder for the About page. Content will be added later.</p>
+    </div>
+  )
+}
