b93069f0d72bbb9523104552f3cd91d7ab2bf050
nick maxwell <drnickmaxwell@gmail.com>
2025-09-15 03:28:03 -0700

    fix: add Contact page skeleton to fix module error

---
+++ ST_MARYS_FRAMEWORK/app/contact/page.tsx
@@ -0,0 +1,9 @@
+
+export default function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Get in touch with St Mary’s House Dental Care. This page is under construction.</p>
+    </div>
+  );
+}
