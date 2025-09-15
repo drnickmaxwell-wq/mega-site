4564387ee877579199a6996d2562872fdbdec234
nick maxwell <drnickmaxwell@gmail.com>
2025-09-15 03:23:03 -0700

    fix: add About page skeleton to fix module error

---
+++ ST_MARYS_FRAMEWORK/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Welcome to St Mary’s House Dental Care. This page is under construction.</p>
+    </div>
+  );
+}
