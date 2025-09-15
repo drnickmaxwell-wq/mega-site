fa2f6adc580f45b7a5363060aa986082063269bf
nick maxwell <drnickmaxwell@gmail.com>
2025-09-15 03:34:22 -0700

    fix: add treatments page skeleton

---
+++ ST_MARYS_FRAMEWORK/app/treatments/page.tsx
@@ -0,0 +1,8 @@
+export default function TreatmentsPage() {
+  return (
+    <div>
+      <h1>Treatments</h1>
+      <p>Information about treatments will be added here. This page is under construction.</p>
+    </div>
+  );
+}
