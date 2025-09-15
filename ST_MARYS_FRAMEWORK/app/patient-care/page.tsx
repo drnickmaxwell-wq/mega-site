2bf7a7283b4aa5fd6ada154ea7d48bcba3b0827d
nick maxwell <drnickmaxwell@gmail.com>
2025-09-15 03:31:26 -0700

    fix: add patient care page skeleton

---
+++ ST_MARYS_FRAMEWORK/app/patient-care/page.tsx
@@ -0,0 +1,9 @@
+h
+export default function PatientCarePage() {
+  return (
+    <div>
+      <h1>Patient Care</h1>
+      <p>Information about patient care will be added here. This page is under construction.</p>
+    </div>
+  );
+}
