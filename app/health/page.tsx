a2b304cda5d06e79b243cd08d6bc409a94ae35bd
nick maxwell <drnickmaxwell@gmail.com>
2025-09-16 11:20:28 -0700

    chore: add health page

---
+++ app/health/page.tsx
@@ -0,0 +1,8 @@
+export default function HealthPage() {
+  return (
+    <main style={{padding:'2rem',maxWidth:720,margin:'0 auto'}}>
+      <h1>Build Health</h1>
+      <p>OK</p>
+    </main>
+  );
+}
